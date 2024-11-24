08b0fb7a76f12a1bf5909d695358ee71e6e95cc7
fajri-farid <fajrijry14@gmail.com>
2024-11-25 00:51:52 +0800

    feat: connect front-end to back-end API for user registration

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,53 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import "../App.css";
+import { Toaster, toast } from "react-hot-toast";
 
 const Register = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
 
   const togglePassword = () => {
     setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      console.log("Kata sandi dan konfirmasi kata sandi tidak cocok");
+      return;
+    }
+
+    const data = {
+      email: email,
+      userName: userName,
+      password: password,
+    };
+
+    try {
+      const response = await axios.post("http://localhost:8080/regist", data);
+      if (response.status === 200) {
+        toast.success("Registrasi berhasil!");
+        setTimeout(() => {
+          navigate("/login");
+        }, 2000);
+      }
+    } catch (err) {
+      if (err.response) {
+        console.log("Error response:", err.response.data);
+        console.log("Error status:", err.response.status);
+      } else if (err.request) {
+        console.log("No response from server:", err.request);
+      } else {
+        console.log("Request error:", err.message);
+      }
+    }
   };
 
   return (
@@ -14,7 +55,7 @@ const Register = () => {
       <div className="flex-grow flex items-center justify-center bg-[#EEEEEE]">
         <div className="text-center flex flex-col gap-10 w-full max-w-md">
           <h1 className="text-4xl font-bold text-[#49A47A]">Regist</h1>
-          <form className="text-md">
+          <form className="text-md" onSubmit={handleSubmit}>
             <div className="mb-4">
               <label className="block text-left mb-2 text-[#49A47A] ml-6">
                 Nama Pengguna
@@ -22,6 +63,8 @@ const Register = () => {
               <input
                 type="text"
                 className="pl-6 rounded-full p-3 w-full bg-[#52B788] bg-opacity-60 border-2 border-[#49A47A] border-opacity-90 focus:outline-none focus:ring-0"
+                onChange={(e) => setUserName(e.target.value)}
+                value={userName}
               />
             </div>
             <div className="mb-4">
@@ -31,6 +74,8 @@ const Register = () => {
               <input
                 type="email"
                 className="pl-6 rounded-full p-3 w-full bg-[#52B788] bg-opacity-60 border-2 border-[#49A47A] border-opacity-90 focus:outline-none focus:ring-0"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="mb-4">
@@ -41,6 +86,8 @@ const Register = () => {
                 <input
                   type={showPassword ? "text" : "password"}
                   className="pl-6 rounded-full p-3 w-full bg-[#52B788] bg-opacity-60 border-2 border-[#49A47A] border-opacity-90 focus:outline-none focus:ring-0"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
                 <button
                   type="button"
@@ -62,6 +109,8 @@ const Register = () => {
                 <input
                   type={showPassword ? "text" : "password"}
                   className="pl-6 rounded-full p-3 w-full bg-[#52B788] bg-opacity-60 border-2 border-[#49A47A] border-opacity-90 focus:outline-none focus:ring-0"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
                 />
                 <button
                   type="button"
@@ -103,6 +152,14 @@ const Register = () => {
         />
         <div className="absolute inset-0 bg-[#95BB72] bg-opacity-20"></div>
       </div>
+
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 3000,
+        }}
+      />
     </div>
   );
 };
