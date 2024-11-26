753e31a9a65505660f3d2737b1703b603a61d025
fajri-farid <fajrijry14@gmail.com>
2024-11-27 06:12:36 +0800

    feat: profile done

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,60 @@
+import React, { useState, useEffect } from "react";
 import "../styles/profile.css";
+import ProfileComp from "../components/profileComp/ProfileComp";
+import Security from "../components/profileComp/Security";
+import axios from "axios";
+import Notification from "../components/profileComp/Notification";
+import ChangePassword from "../components/profileComp/ChangePassword";
 
 const Profile = () => {
+  const [activeMenu, setActiveMenu] = useState("profile");
+  const [isChangingPassword, setIsChangingPassword] = useState(false);
+  const [profile, setProfile] = useState({
+    userName: "",
+    profilePicture: "",
+  });
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/profile", {
+          withCredentials: true,
+        });
+
+        if (response.data.success) {
+          const { userName, profilePicture } = response.data.data;
+          setProfile({ userName, profilePicture });
+        }
+      } catch (error) {
+        console.error("Error fetching profile:", error);
+      }
+    };
+
+    fetchProfile();
+  }, []);
+
+  const renderMenuComponent = () => {
+    if (isChangingPassword) {
+      return <ChangePassword />;
+    }
+
+    switch (activeMenu) {
+      case "profile":
+        return <ProfileComp />;
+      case "notifications":
+        return <Notification />;
+      case "security":
+        return (
+          <Security onChangePassword={() => setIsChangingPassword(true)} />
+        );
+      default:
+        return <ProfileComp />;
+    }
+  };
+
   return (
-    <div className="w-full flex justify-center items-center h-[80%]">
-      <div className="box mt-10 bg-white w-[95%] max-w-[1500px] h-[100%] rounded-md shadow-md p-8 flex">
+    <div className="w-full flex justify-center items-center h-full">
+      <div className="box mt-10 bg-white w-[95%] max-w-[1500px] h-[100%] rounded-md shadow-md p-12 flex">
         <div className="w-1/4 pr-4 border-r">
           <div className="flex flex-col items-center">
             <img
@@ -11,105 +62,41 @@ const Profile = () => {
               alt="Profile"
               className="rounded-full mb-4 w-40 h-40 object-cover"
             />
-            <h2 className="font-bold text-lg">Fajri Wajdi</h2>
+            <h2 className="font-bold text-lg">{profile.userName}</h2>
           </div>
           <ul className="mt-8">
-            <li className="mb-4 p-2 bg-gray-200 rounded-md">Profil</li>
-            <li className="mb-4 p-2 hover:bg-gray-100 rounded-md">
+            <li
+              className={`mb-4 p-2 ${
+                activeMenu !== "profile" ? "" : "bg-gray-200"
+              } rounded-md cursor-pointer`}
+              onClick={() => setActiveMenu("profile")}
+            >
+              Profil
+            </li>
+            <li
+              className={`mb-4 p-2 ${
+                activeMenu !== "notifications" ? "" : "bg-gray-200"
+              } rounded-md cursor-pointer`}
+              onClick={() => setActiveMenu("notifications")}
+            >
               Notifikasi
             </li>
-            <li className="mb-4 p-2 hover:bg-gray-100 rounded-md">
-              Riwayat Donasi
-            </li>
-            <li className="mb-4 p-2 hover:bg-gray-100 rounded-md">
-              Kata Sandi & Keamanan
+            <li
+              className={`mb-4 p-2 ${
+                activeMenu !== "security" ? "" : "bg-gray-200"
+              } rounded-md cursor-pointer`}
+              onClick={() => setActiveMenu("security")}
+            >
+              Keamanan & Kata Sandi
             </li>
             <li className="p-2 text-red-500 hover:underline cursor-pointer">
               Keluar
             </li>
           </ul>
         </div>
-        <div className="w-3/4 pl-4">
-          <div className="flex justify-between items-center mb-6">
-            <h1 className="text-2xl font-bold">Profil</h1>
-            <button className="bg-blue-400 text-white px-4 py-2 rounded-md">
-              Edit Profil
-            </button>
-          </div>
-          <form>
-            <div className="grid grid-cols-2 gap-4">
-              <div>
-                <label className="block text-gray-600 mb-2">
-                  Nama Pengguna
-                </label>
-                <input
-                  type="text"
-                  className="w-full p-2 border rounded-md bg-gray-100"
-                />
-              </div>
-              <div>
-                <label className="block text-gray-600 mb-2">
-                  Tanggal Lahir
-                </label>
-                <input
-                  type="date"
-                  className="w-full p-2 border rounded-md bg-gray-100"
-                />
-              </div>
-              <div>
-                <label className="block text-gray-600 mb-2">Email</label>
-                <input
-                  type="email"
-                  className="w-full p-2 border rounded-md bg-gray-100"
-                />
-              </div>
-              <div>
-                <label className="block text-gray-600 mb-2">Gender</label>
-                <select className="w-full p-2 border rounded-md bg-gray-100">
-                  <option>Laki-laki</option>
-                  <option>Perempuan</option>
-                </select>
-              </div>
-              <div>
-                <label className="block text-gray-600 mb-2">Nama Lengkap</label>
-                <input
-                  type="text"
-                  className="w-full p-2 border rounded-md bg-gray-100"
-                />
-              </div>
-              <div>
-                <label className="block text-gray-600 mb-2">Alamat</label>
-                <input
-                  type="text"
-                  className="w-full p-2 border rounded-md bg-gray-100"
-                />
-              </div>
-              <div>
-                <label className="block text-gray-600 mb-2">Bio</label>
-                <input
-                  type="text"
-                  className="w-full p-2 border rounded-md bg-gray-100"
-                />
-              </div>
-              <div>
-                <label className="block text-gray-600 mb-2">
-                  Nomor Handphone
-                </label>
-                <input
-                  type="text"
-                  className="w-full p-2 border rounded-md bg-gray-100"
-                />
-              </div>
-            </div>
-            <button
-              type="submit"
-              className="mt-10 w-full bg-gray-300 text-gray-500 px-4 py-2 rounded-md cursor-not-allowed"
-              disabled
-            >
-              Simpan Perubahan
-            </button>
-          </form>
-        </div>
+
+        {/* Content */}
+        <div className="w-3/4 pl-4">{renderMenuComponent()}</div>
       </div>
     </div>
   );
